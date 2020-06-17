143b708367ef46228f8428af843edbd0f17bbe89
kzuo2 <kzuo2@wisc.edu>
2020-06-17 23:24:04 +0200

    cast method in another method

--- src/Part1.java
+++ src/Part1.java
@@ -1,25 +1,37 @@
 public class Part1 {
+
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-//       The error is resolved by toggling the allow-unrelated-histories switch. After a git pull or git merge command, add the following tag:
-//
-//git pull origin master --allow-unrelated-histories
-//More information can be found here, ​ on Git’s official documentation.
+
+        public void testSimpleGene (String dna) {
+            String first = "cccatggggtttaaataataataggagagagagagagagttt";
+            String second = "atggggtttaaataataatag";
+            String third = "atgcctag";
+            String fourth = "";
+            String fifth = "ATGCCCTAG";
+            String sixth = "ATGCCCTAG";
+            String result = findSimpleGene(dna);
+
+
+
+        }
     }
 
-    public String findSimpleGene (String dna){
+    public String findSimpleGene(String dna) {
         int start = dna.indexOf("ATG");
         if (start == -1) {
             return "There is no ATG";
         }
-        int stop = dna.indexOf("TAA", start+3);
+        int stop = dna.indexOf("TAA", start + 3);
         if (stop == -1) {
             return "There is no TAA";
         }
-
-
-
-
+        if ((stop - start) % 3 == 0) {
+            return dna.substring(start, stop + 3);
+        }
     }
-
 }
+
+
+
+
+
