566d827589668c929dc8ba70cc96cbcf31dbc56b
kzuo2 <kzuo2@wisc.edu>
2020-06-17 23:02:26 +0200

    error in git creating repo

--- src/Part1.java
+++ src/Part1.java
@@ -1,6 +1,10 @@
 public class Part1 {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+//       The error is resolved by toggling the allow-unrelated-histories switch. After a git pull or git merge command, add the following tag:
+//
+//git pull origin master --allow-unrelated-histories
+//More information can be found here, ​ on Git’s official documentation.
     }
 
     public String findSimpleGene (String dna){
