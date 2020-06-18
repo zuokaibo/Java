e63c84ceec10e504a5b2b7fdde4a7902f7899ceb
kzuo2 <kzuo2@wisc.edu>
2020-06-18 18:22:39 +0200

    strusture of method cast in another method and how to call it

--- src/Part1.java
+++ src/Part1.java
@@ -1,33 +1,46 @@
 public class Part1 {
 
     public static void main(String[] args) {
-
-        public void testSimpleGene (String dna) {
-            String first = "cccatggggtttaaataataataggagagagagagagagttt";
-            String second = "atggggtttaaataataatag";
-            String third = "atgcctag";
-            String fourth = "";
-            String fifth = "ATGCCCTAG";
-            String sixth = "ATGCCCTAG";
-            String result = findSimpleGene(dna);
-
-
-
-        }
+        testSimpleGene();
     }
 
-    public String findSimpleGene(String dna) {
+    public static void testSimpleGene() {
+        String first = "CCCATGGGGTTTAAATAATAATAGGAGAGAGAGAGAGAGTTT";
+        String second = "ATGGGGTTTAAATAATAATAG";
+        String third = "ATGCCTAG";
+        String fourth = "";
+        String fifth = "ATGCCCTAG";
+        String result3;
+        String result4;
+        String result5;
+        String result2;
+        result3 = findSimpleGene(third);
+        result4 = findSimpleGene(fourth);
+        result5 = findSimpleGene(fifth);
+        result2 = findSimpleGene(second);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result2);
+    }
+
+    public static String findSimpleGene(String dna) {
+
         int start = dna.indexOf("ATG");
         if (start == -1) {
             return "There is no ATG";
         }
+
         int stop = dna.indexOf("TAA", start + 3);
         if (stop == -1) {
             return "There is no TAA";
         }
+
         if ((stop - start) % 3 == 0) {
             return dna.substring(start, stop + 3);
         }
+
+        return dna;
     }
 }
 
